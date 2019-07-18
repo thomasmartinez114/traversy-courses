8d772a3f260a73f1a87b5aea4ad383c810cd8846
tmartinez114 <tmartinez114@gmail.com>
2019-07-18 11:45:58 -0400

    In users.js - pulled in User model, and prepped the try and catch

--- mern-stack/devconnector/routes/api/users.js
+++ mern-stack/devconnector/routes/api/users.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
 
+// Pulling User model
+const User = require('../../models/User');
+
 // @route   POST api/users
 // @desc    Register user
 // @access  Public
@@ -17,13 +20,27 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('User router');
+    const { name, email, password } = req.body;
+
+    try {
+      // See if user exists
+
+      // Get users gravatar
+
+      // Encrypt password
+
+      // Return jsonwebtoken
+      res.send('User router');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
